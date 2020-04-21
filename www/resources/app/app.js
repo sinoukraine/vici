22120218463a7acdbcd895af38461cd551a54f7d
DimiUA <s.dimi.d@gmil.com>
2020-04-21 20:58:52 +0300

    asdasdasd

--- www/resources/app/app.js
+++ www/resources/app/app.js
@@ -792,8 +792,8 @@ let app = new Framework7({
         displayNewNotificationArrived: function(message){
             let self = this;
             let formattedMsgList = self.methods.formatNotifications([message]);
-console.log(message)
-            console.log(messformattedMsgListage)
+            self.dialog.alert(message);
+            self.dialog.alert(formattedMsgList);
             self.notification.create({
                 title: self.name,
                 titleRightText: LANGUAGE.COM_MSG062, //now
